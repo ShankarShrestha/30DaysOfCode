5eb9c4bd4734a3e6fea99f45a8a2dfbe23e9824f
Shankar Shrestha <nepzeal@gmail.com>
2019-02-05 20:22:27 -0600

    new Day 1

---
+++ Day1/Average.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DayCode.Day1
+{
+    class Average
+    {
+        public void FindAverage()
+        {
+            Console.Write("Enter the First Number: ");
+            int inputNumber1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the Second Number: ");
+            int inputNumber2 = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the Third Number: ");
+            int inputNumber3 = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the Forth Number: ");
+            int inputNumber4 = Int32.Parse(Console.ReadLine());
+
+            float average = (inputNumber1 + inputNumber2 + inputNumber3 + inputNumber4) / 4;
+
+            Console.WriteLine("The average is: " + average);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day1/LongWord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DayCode.Day1
+{
+    class LongWord
+    {
+        public void FindLongWord()
+        {
+            Console.WriteLine("Please Enter the Sentence: ");
+            string inputSentence = Console.ReadLine();
+
+            // Convert the input sentence in array of word.
+            string[] inputArray = inputSentence.Split(' ');
+
+            string longWord = inputArray[0];
+            for (int i = 1; i < inputArray.Length; i++)
+            {
+                if (longWord.Length < inputArray[i].Length)
+                {
+                    longWord = inputArray[i];
+                }
+            }
+            Console.WriteLine("The Longest word is: " + longWord);
+            Console.ReadLine();
+        }
+    }
+}
